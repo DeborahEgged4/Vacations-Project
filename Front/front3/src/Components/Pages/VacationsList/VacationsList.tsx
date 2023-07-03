6b2a6b2f6e6c43ac246b9f488b5b1ee0de125b87
Deborah Egged <deborahegged.git@gmail.com>
2023-07-03 14:16:57 +0300

    second

--- Front/front3/src/Components/Pages/VacationsList/VacationsList.tsx
+++ Front/front3/src/Components/Pages/VacationsList/VacationsList.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+/*import {useEffect, useState} from "react";
 import IVacation from "../../../interfaces/Vacation";
 import "./VacationsList.css";
 
@@ -62,5 +62,93 @@ const VacationsList = (props: {isAdmin: boolean}) => {
     );
 }
 
+export default VacationsList;*/
+
+import React, { useEffect, useState } from "react";
+import IVacation from "../../../interfaces/Vacation";
+import "./VacationsList.css";
+import ReactPaginate from "react-paginate";
+
+const VacationsList = (props: { isAdmin: boolean }) => {
+  console.log(`props.isAdmin: ${props.isAdmin}`);
+  const [vacations, setVacation] = useState<IVacation[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const perPage = 10;
+
+  const getVacations = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/vacation/vacations");
+      const vacationsResponse = await response.json();
+      console.log(vacationsResponse);
+      setVacation(vacationsResponse);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getVacations();
+  }, []);
+
+  const handlePageChange = (selectedPage: { selected: number }) => {
+    setCurrentPage(selectedPage.selected);
+  };
+
+  const offset = currentPage * perPage;
+  const currentVacations = vacations.slice(offset, offset + perPage);
+
+  return (
+    <div>
+<ReactPaginate
+  breakLabel={null}
+  pageCount={Math.ceil(vacations.length / perPage)}
+  marginPagesDisplayed={2}
+  pageRangeDisplayed={vacations.length > perPage ? vacations.length / perPage : 1}
+  onPageChange={handlePageChange}
+  containerClassName={"pagination"}
+  activeClassName={"active"}
+  pageClassName={"page-item"}
+  pageLinkClassName={"page-link"}
+/>
+      <div
+        className="VacationsList"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "10px",
+        }}
+      >
+        {currentVacations.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              border: "1px solid black",
+              borderRadius: "10px",
+              width: "200px",
+              backgroundColor: "rgba(169,169,169,0.57)",
+            }}
+          >
+            <h3>Destination:{item.destination}</h3>
+            <img src={item.image} width={"150px"} alt={item.destination} />
+            <p>Description{item.description}</p>
+            <p>Price:{item.price}</p>
+            <p>Start Date: {new Date(item.startDate).toLocaleDateString()}</p>
+            <p>End Date: {new Date(item.endDate).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
 export default VacationsList;
+
+
+
+
+
+
+
+
 
